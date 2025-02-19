bd49133c9627cd507e20b9c9aea0bdecf32831f8
tomByrer <tombyrer@gmail.com>
2025-02-19 05:11:42 -0600

    swap demo JS

---
+++ packages/simple-state-shifter/demos/00-countdown-timer.plain..js
@@ -0,0 +1,133 @@
+/* countdown-timer v00.04; plain JS (c) 2025 Tom Byrer
+
+This is to show how in some languages implement a 'State Machine'
+without using a state machine library, with only conditional statements.
+Sometimes a FSM is not so obvious like this, the condtions
+are spread out over 100+ lines of code.
+*/
+/* 
+stateDiagram
+  [*] --> setting
+  setting --> running:start
+  running --> setting:delete
+  running --> alarm:expire
+  running --> paused:pause
+  running --> standby:reset
+  paused --> setting:delete
+  paused --> reset:standby
+  paused --> running:resume
+  alarm --> setting:delete
+  alarm --> standby:stop
+  standby --> setting:delete
+  standby --> running:start
+*/
+
+// simulate 'store state' outside of the FSM for intergration
+// with other state libraries or FE frameworks
+// data must have get(key) & set(key, value) methods
+const data = new Map([['state', '']])
+
+class TimerStateMachine {
+  constructor() {
+    data.set('state', 'setting') // inital state
+  }
+
+trigger(event) {
+  switch (data.get('state')) {
+
+    case 'setting':
+      if (event === 'start') {
+        data.set('state', 'running')
+      }
+      break;
+
+    case 'running':
+      if (event === 'delete') {
+        data.set('state', 'setting')
+      } else if (event === 'expire') {
+        data.set('state', 'alarm')
+      } else if (event === 'pause') {
+        data.set('state', 'paused')
+      } else if (event === 'reset') {
+        data.set('state', 'standby')
+      }
+      break;
+
+    case 'paused':
+      if (event === 'delete') {
+        data.set('state', 'setting')
+      } else if (event === 'reset') {
+        data.set('state', 'standby')
+      } else if (event === 'resume') {
+        data.set('state', 'running')
+      }
+      break;
+
+    case 'alarm':
+      if (event === 'delete') {
+        data.set('state', 'setting')
+      } else if (event === 'stop') {
+        data.set('state', 'standby')
+      }
+      break;
+
+    case 'standby':
+      if (event === 'delete') {
+        data.set('state', 'setting')
+      } else if (event === 'start') {
+        data.set('state', 'running')
+      }
+      break;
+
+    default:
+      data.set('state', 'setting')
+      break;
+  }
+
+}
+
+  get() {
+    return data.set('state')
+  }
+}
+
+// Example usage
+const machine = new TimerStateMachine()
+/*^ end finite state machine */
+
+
+// automatically run demostration
+
+console.log(`Running plain JS version of countdown-timer:`)
+machineSequence(
+  machine, data,
+  [
+    'reset', // noop; should be in setting already
+    'start', // setting -> running
+    'pause', // running -> paused
+    'resume', // paused -> running
+    'expire', // running -> alarm
+    'stop', // alarm -> standby
+    'start', // standby -> running
+    'reset', // running -> stanby
+    'delete', // stanby -> setting
+  ],
+)
+
+/* run though a machine via an array of triggers */
+function machineSequence(machine, data, sequence=[]){
+  console.log('init entry state:', data.get('state'), '\n')
+  if (data.effect){
+    data.effect(() => {
+      console.log(` e: ${data.get('state')}`);
+    })
+  }
+  const len = sequence.length
+  for (let i=0; i<len; i++) {
+    console.log(i, 'trigger:', sequence[i])
+    machine.trigger(sequence[i])
+    console.log(i,
+      'state:', data.get('state'), '\n',
+    )
+  }
+}
---
+++ packages/simple-state-shifter/demos/01-countdown-timer.fsm.js
@@ -0,0 +1,81 @@
+/* countdown-timer v01.03 (c) 2025 Tom Byrer */
+/* https://www.mermaidchart.com/play#pako:eNp9UDFuwzAM_ArhsYA-wCFTxk5Z6wysxdhCbcqQaCRB0L_Hoh3AaOBOIu94dyIfVRM9V1g552ppolxCi7UAaMcDI1xi4qwF6GP8QehI_DHRVQyie5wUwVObuBazyErKxzAjNJQZHxI3GqLA56n0Xx9ncO4AmVWDtAVaS4PTJFJqhNkoWfAL2qjQc8_Kf1nqKQ3ItzGkN26kKbNHe95cdd7q-47zqmyRy_BO4pb8R7gGFG4aTGjf2zHdcKtn1jjadZZ-R7dlX4nr5arfJxGwpRE
+stateDiagram
+  [*] --> setting
+  setting --> running:start
+  running --> setting:delete
+  running --> alarm:expire
+  running --> paused:pause
+  running --> standby:reset
+  paused --> setting:delete
+  paused --> reset:standby
+  paused --> running:resume
+  alarm --> setting:delete
+  alarm --> standby:stop
+  standby --> setting:delete
+  standby --> running:start
+*/
+import createMachine from '../simple-state-shifter'
+
+export const states ={
+  setting: {  // 1st screen, no timer set
+    start: 'running', // completed setting timer time
+  },
+  running: {
+    delete: 'setting',
+    expire: 'alarm', // countdown reached 0
+    pause: 'paused', // stop countdown, current value is on hold
+    reset: 'standby', // stop countdown, return to after the time is set
+  },
+  paused: {
+    delete: 'setting',
+    reset: 'standby',
+    resume: 'running',
+  },
+  alarm: {
+    delete: 'setting',
+    stop: 'standby',
+  },
+  standby: { // timer reset, awaiting to start
+    delete: 'setting',
+    start: 'running',
+  },
+}
+export const machine = createMachine(states)
+/*^ end finite state machine */
+
+// automatically run demostration
+console.log(`Running simple-state-shifter version of countdown-timer,
+output should equal 'plain' JS version:`)
+
+// function getAllValues(obj) {
+//   let values = [];
+//   for (let key in obj) {
+//     if (obj.hasOwnProperty(key)) {
+//       if (typeof obj[key] === 'object') {
+//         values = values.concat(getAllValues(obj[key]));
+//       } else {
+//         values.push(obj[key]);
+//       }
+//     }
+//   }
+//   return values;
+// }
+
+// console.log([...new Set(getAllValues(states))] ); // Output: [1, 2, 3, 4, 5, 6]
+
+import { machineSequence } from '../../../utils'
+machineSequence(
+  machine, machine.data,
+  [
+    'reset', // noop; should be in setting already
+    'start', // setting -> running
+    'pause', // running -> paused
+    'resume', // paused -> running
+    'expire', // running -> alarm
+    'stop', // alarm -> standby
+    'start', // standby -> running
+    'reset', // running -> stanby
+    'delete', // stanby -> setting
+  ],
+)
